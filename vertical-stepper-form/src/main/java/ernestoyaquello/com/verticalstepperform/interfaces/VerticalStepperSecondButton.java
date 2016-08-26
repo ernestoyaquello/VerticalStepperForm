7694a12a11ab7e984cabdf071431fa2cd044b852
Tobias <tobys.briese@yahoo.de>
2016-08-26 17:55:08 +0200

    fixed javadoc error

--- vertical-stepper-form/src/main/java/ernestoyaquello/com/verticalstepperform/interfaces/VerticalStepperSecondButton.java
+++ vertical-stepper-form/src/main/java/ernestoyaquello/com/verticalstepperform/interfaces/VerticalStepperSecondButton.java
@@ -4,7 +4,7 @@ public interface VerticalStepperSecondButton {
 
     /**
      * This method will be called when the user press the second button in last step. That
-     * button can be added via {@link ernestoyaquello.com.verticalstepperform.VerticalStepperFormLayout.Builder#addSecondButtonLastStep(String) addSecondButtonLastStep}
+     * button can be added via {@link ernestoyaquello.com.verticalstepperform.VerticalStepperFormLayout.Builder#addSecondButtonLastStep(String, VerticalStepperSecondButton) addSecondButtonLastStep}
      */
     void onClickSecondButton();
 }
